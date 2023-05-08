017f12fadadfd391752b39de0e95690d91bd5b11
Mateusz Zajonz <zajonzmateusz@gmail.com>
2023-05-08 21:16:21 +0200

    Initial commit

---
+++ src/test/java/pl/zajonz/currencyprovider/CurrencyProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zajonz.currencyprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
